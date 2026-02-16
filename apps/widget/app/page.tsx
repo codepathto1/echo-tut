3af169a49682fdd4b75d65a3bafc8de9d0e6a4fb
Jason <jason@jason.com>
2026-02-16 10:42:58 +0530

    Initial setup

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,14 @@
 import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
+import { subtract } from "@workspace/math/add";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">App/Web</h1>
+        <h1 className="text-2xl font-bold">App/Widget</h1>
         <div className="flex gap-2">
           <Button>Button</Button>
-          <Button variant="outline">Outline</Button>
-          <p>{add(2, 8)}</p>
+          {subtract(2, 8)}
         </div>
       </div>
     </div>
