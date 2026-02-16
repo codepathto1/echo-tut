50c3a077fa9f18f5371131b946ea4a4b66660ac9
Jason <jason@jason.com>
2026-02-16 23:08:06 +0530

    03 Clerk Integration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
-
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
+
 import { useQuery } from "convex/react";
 import { useMutation } from "convex/react";
+
+import { Authenticated, Unauthenticated } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Page() {
   const users = useQuery(api.users.getAllUser);
   const addUser = useMutation(api.users.addUsers);
@@ -12,11 +16,16 @@ export default function Page() {
       <div>
         <h2>app/web</h2>
       </div>
-
-      <div className="flex flex-col max-w-sm wfull">
-        <Button onClick={() => addUser()}>Add</Button>
-        {JSON.stringify(users, null, 2)}
-      </div>
+      <Authenticated>
+        <div className="flex flex-col max-w-sm wfull">
+          <Button onClick={() => addUser()}>Add</Button>
+          {JSON.stringify(users, null, 2)}
+        </div>
+        <UserButton />
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
     </div>
   );
 }
--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -2,8 +2,18 @@
 
 import React from "react";
 import { ConvexReactClient, ConvexProvider } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { useAuth } from "@clerk/nextjs";
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error("Missing  NEXT_PUBLIC_CONVEX_URL");
+}
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL! || "");
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
 }
