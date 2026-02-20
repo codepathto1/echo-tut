25a155f3ab688e20a332b2f0422140903e803c13
Jason <jason@jason.com>
2026-02-20 19:35:38 +0530

    09-Widget layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,20 +1,16 @@
 "use client";
 
-import { Button } from "@workspace/ui/components/button";
-import { useQuery } from "convex/react";
-import { api } from "@workspace/backend/_generated/api";
+import { use } from "react";
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
 
-export default function Page() {
-  const users = useQuery(api.users.getAllUser);
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">App/Widget</h1>
-        <div className="flex gap-2">
-          <Button>Button</Button>
-        </div>
-        <div className="flex flex-col max-w-sm w-full">{JSON.stringify(users, null, 2)}</div>
-      </div>
-    </div>
-  );
+import { Button } from "@workspace/ui/components/button";
+interface Props {
+  searchParams: Promise<{
+    organizationId: string;
+  }>;
+}
+
+export default function Page({ searchParams }: Props) {
+  const { organizationId } = use(searchParams);
+  return <WidgetView organizationId={organizationId} />;
 }
